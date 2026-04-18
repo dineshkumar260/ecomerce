2763deb71293c0a49e2c6272ffe53bab741faf51
dineshkumar260 <dineshkumarsara07@gmail.com>
2026-04-18 09:37:35 +0530

    Migration to Vercel Services: Added root vercel.json and relative API paths

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ecommerce-ssew.onrender.com/api',
+    baseURL: '/_/backend/api',
 });
 
 api.interceptors.request.use(config => {
